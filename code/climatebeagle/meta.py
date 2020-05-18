46b255d1f06a78f47ced74127f8f80e0253b9641
climatebeagle <climatebeagle>
2020-05-18 10:43:52 -0700

    Initial map with stations

---
+++ code/climatebeagle/meta.py
@@ -0,0 +1,33 @@
+import json
+import math
+import pandas as pd
+
+with open('station-listing.html') as f:
+    dfs = pd.read_html(io=f)
+
+df = dfs[0]
+
+crn = df[df['NETWORK'] == 'CRN']
+stations = []
+for idx, station in crn.iterrows():
+    if str(station['CLOSED DATE']) != 'nan':
+        continue
+    conus = True
+    if station['STATION'].startswith('AK'):
+       conus = False
+    if station['STATION'].startswith('HI'):
+       conus = False
+
+    js = {}
+    js['name'] = station['STATION']
+    js['state'] = station['STATION'].split()[0]
+    #js['start_date'] = station['STATION']
+    js['lat'] = station['LATITUDE']
+    js['lon'] = station['LONGITUDE']
+    js['conus'] = conus
+    stations.append(js)
+
+with open('../../src/data/stations.js', 'w') as sf:
+    sf.write("uscrn_stations = ")
+    json.dump(stations, sf)
+    sf.write(";\n")
