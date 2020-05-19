18dcb37a5be3238fa3f6f499e7926b6220ed947f
climatebeagle <climatebeagle>
2020-05-19 07:30:34 -0700

    Change bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'USCRN Thoughts',
+    author: 'climatebeagle',
+    description: 'Looking at US Climate Reference Network.',
+    siteUrl: 'https://uscrn-thoughts.netlify.app',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/uscrn-thoughts',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
