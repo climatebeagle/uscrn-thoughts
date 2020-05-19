56358b0d1854d12a17c1afb39f7fbafe17777cf4
climatebeagle <climatebeagle>
2020-05-19 08:27:36 -0700

    Extract js

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,19 +17,9 @@ class Map extends React.Component {
       >
     <h2>USCRN Stations</h2>
     <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.3.1/build/ol.js"></script>
+    <script src="maps/map.js"></script>
     <script type="text/javascript">
-      var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([-98.579, 39.828]),
-          zoom: 4
-        })
-      });
+      var map = createUSCRNMap();
     </script>
     </div>
     )
