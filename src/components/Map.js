b1283bdde04cb7342c60275235999f2ce89df655
climatebeagle <climatebeagle>
2020-05-19 08:05:50 -0700

    Add locations

---
+++ src/components/Map.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+    <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+    <h2>USCRN Stations</h2>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.3.1/build/ol.js"></script>
+    <script type="text/javascript">
+      var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          }),
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([-98.579, 39.828]),
+          zoom: 4
+        })
+      });
+    </script>
+    </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/locations.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+import { rhythm } from '../utils/typography'
+
+class Locations extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    return (
+      <div>
+        <Helmet title={siteTitle} />
+        <Map />
+      </div>
+    )
+  }
+}
+
+export default Locations
