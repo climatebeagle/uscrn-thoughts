0ba39c3a9965bcfce3d94994928adabb1ff28807
climatebeagle <climatebeagle>
2020-05-28 06:56:04 -0700

    Add hovers

--- src/components/maps/map.js
+++ src/components/maps/map.js
@@ -5,9 +5,55 @@
 
 import { uscrn_stations } from '../data/stations'
 
+function showUSCRN(overlay, map, content, coordinate) {
+    var features = map.getFeaturesAtPixel(map.getPixelFromCoordinate(coordinate), {
+        hitTolerance: 2
+    });
+    if (features.length < 1) {
+        overlay.setPosition(undefined);
+        return;
+    }
+    var feature = features[0];
+    content.innerHTML = hoverHTML(feature);
+    overlay.setPosition(coordinate);
+}
+
+function hoverHTML(feature) {
+  var name = feature.values_.uscrn.name;
+  var text = '&nbsp;' + name + '&nbsp;';
+  return text;
+}
+
+function createPopup(ol, map) {
+  var content = document.createElement('div');
+  content.style.overflow = "auto";
+  content.style.height = "90px";
+
+  var popup = document.createElement('div');
+  popup.className = "ol-unselectable"
+  popup.style.zindex = "1";
+  popup.style.position = "absolute";
+  popup.style.background = 'FUCHSIA';
+  popup.style.font = "18px Calibri,sans-serif";
+  popup.style.color = "white";
+ 
+popup.appendChild(content);
+
+var overlay = new ol.Overlay({
+    element: popup,
+   // positioning: 'bottom-center',
+    offset: [0, -95],
+    autoPan: false
+});
+map.addOverlay(overlay);
+map.on('pointermove', function(event){ showUSCRN(overlay, map, content, event.coordinate); });
+
+}
+
 function addStation(ol, olp, olg, station, style) {
     var sf = new ol.Feature({
-      geometry: new olg.Point(olp.fromLonLat([station.lon, station.lat]))
+      geometry: new olg.Point(olp.fromLonLat([station.lon, station.lat])),
+      uscrn: station,
     });
 
     sf.setStyle(style)
@@ -45,7 +91,7 @@ export function createUSCRNMap() {
     import("ol/proj").then((olp) => {
     import("ol/style").then((olstyle) => {
     import("ol/geom").then((olg) => {
-      new ol.Map({
+      var map = new ol.Map({
         target: 'uscrnmap',
         layers: [
           new oll.Tile({
@@ -58,6 +104,7 @@ export function createUSCRNMap() {
           zoom: 4
         })
       });
+      createPopup(ol, map);
   });
   });
   });
