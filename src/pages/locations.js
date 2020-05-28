c6eb02a1cbfd9d5730c4b57a216e6b5960636236
climatebeagle <climatebeagle>
2020-05-28 07:45:50 -0700

    fix layout

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+//import React from 'react'
+//import get from 'lodash/get'
+//import Helmet from 'react-helmet'
 
 import Layout from "../components/layout"
 import USCRNMap from '../components/uscrnmap'
 
-class Locations extends React.Component {
+// class Locations extends React.Component {
+const Locations = ({ data, location }) => {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     return (
-    <Layout title={siteTitle}>
+    <Layout location={location} title={siteTitle}>
       <div>
         <USCRNMap />
       </div>
